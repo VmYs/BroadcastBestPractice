29044f42a78a23f8e6955430ce1b17f068e7501e
VmYs <ysfrank1@gmail.com>
2022-01-19 14:07:47 +0800

    20220119-7.3

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.broadcastbestpractice
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,16 +11,44 @@ import com.example.broadcastbestpractice.databinding.ActivityMainBinding
 
 class LoginActivity : BaseActivity() {
 
-    lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
 
+    @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember = prefs.getBoolean("remember_password", false)
+        if (isRemember) {
+            val account = prefs.getString("account", "")
+            val password = prefs.getString("password", "")
+            binding.accountEdit.setText(account)
+            binding.passwordEdit.setText(password)
+            binding.rememberPass.isChecked = true
+        }
+
         binding.login.setOnClickListener {
             val account = binding.accountEdit.text.toString()
             val password = binding.passwordEdit.text.toString()
+//            if (account == "admin" && password == "123456") {
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//                finish()
+//            } else {
+//                Toast.makeText(this, "account or password is invalid", Toast.LENGTH_SHORT).show()
+//            }
             if (account == "admin" && password == "123456") {
+                val editor = prefs.edit()
+                if (binding.rememberPass.isChecked) {
+                    editor.putBoolean("remember_password", true)
+                    editor.putString("account", account)
+                    editor.putString("password", password)
+                } else {
+                    editor.clear()
+                }
+                editor.apply()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
